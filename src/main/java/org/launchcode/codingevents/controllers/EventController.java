e29d07b6ae514b183ab4fd902350881dfad4d2f1
John Wasielewicz <wazjohn@yahoo.com>
2020-09-28 20:20:03 -0500

    Expanded events schedule.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
@@ -14,12 +16,19 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
+//        List<String> events = new ArrayList<>();
+//        events.add("Code With Pride");
+//        events.add("Strange Loop");
+//        events.add("Apple WWDC");
+//        events.add("SpringOne Platform");
+//        model.addAttribute("events", events);
+//        return "events/index";
+
+        Map<String, String> eventsCode = new HashMap<>();
+        eventsCode.put("Code With Pride", "LGBT Friendly Coding Meetup");
+        eventsCode.put("Strange Loop", "Loops that are strange");
+        eventsCode.put("Apple WWDC", "Event for people who like overpriced computers");
+        model.addAttribute("events", eventsCode);
         return "events/index";
     }
 
