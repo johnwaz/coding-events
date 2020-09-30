4e8c5d0fc9b10b48169ea58d0ce1f40b1917927e
John Wasielewicz <wazjohn@yahoo.com>
2020-09-30 18:26:36 -0500

    Edit model classes.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -57,4 +57,22 @@ public class EventController {
         }
         return "redirect:";
     }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        Event event = EventData.getById(eventId);
+        model.addAttribute("event", event);
+        if (event != null) {
+            model.addAttribute("title", "Edit Event " + event.getName() + " (ID = " + event.getId() + ")");
+        }
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        Event event = EventData.getById(eventId);
+        event.setName(name);
+        event.setDescription(description);
+        return "redirect:";
+    }
 }
