dc603a70e71f057f858fae5190a33591e87ae344
John Wasielewicz <wazjohn@yahoo.com>
2020-10-24 13:01:27 -0500

    Created new branch

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,10 +1,7 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 @Entity
 public class EventCategory extends AbstractEntity {
